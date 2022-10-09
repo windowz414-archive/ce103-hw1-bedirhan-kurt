121de8503a509b8e473a265b54790b75d85295b7
Bedirhan KURT <bedirhan.kurt.trjp@gmail.com>
2022-10-09 23:42:26 +0300

    hw1: Initial skeleton
    * Add basic files I'll be working on, as well as some generic notices
      regarding copyright and how will they be utilized.
    
    Signed-off-by: Bedirhan KURT <bedirhan.kurt.trjp@gmail.com>

---
+++ collect_user_info.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+REM
+REM Copyright (C) 2022 Bedirhan KURT. All Rights Reserved.
+REM Copyright (C) 2022 Beru "Stella" Hinode on behalf of Bedirhan KURT. All Rights Reserved.
+REM
+REM SPDX-License-Identifity: GPL-2.0-only
+REM
+
+REM This script is a self-made sample with lots of experimentation and
+REM researches to be submitted as part of assignment in Recep Tayyip
+REM Erdogan University, Computer Engineering Department. It is designed
+REM to do as the requested assignment does and emails them to
+REM windowz414@1337.lgbt as proof. Check relevant platform's
+REM requirements.txt file for more info about dependencies and
+REM compatibility. This assignment will stay FOSS (Free and Open Source
+REM Software) and be provided in "AS IS" basis without any guarantees.
+REM Any kinds of contribution, such as forks, PRs, Issues, are welcome
+REM as long as it has the intention of constructive critisism or actual
+REM contribution to source code. This piece of code, in no way, reflects
+REM any of my plans for current and future projects and is meant solely
+REM as a homework/assignment.
---
+++ collect_user_info.sh
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2022 Bedirhan KURT. All Rights Reserved.
+# Copyright (C) 2022 Beru "Stella" Hinode on behalf of Bedirhan KURT. All Rights Reserved.
+#
+# SPDX-License-Identifity: GPL-2.0-only
+#
+
+# This script is a self-made sample with lots of experimentation and
+# researches to be submitted as part of assignment in Recep Tayyip
+# Erdogan University, Computer Engineering Department. It is designed
+# to do as the requested assignment does and emails them to
+# windowz414@1337.lgbt as proof. Check relevant platform's
+# requirements.txt file for more info about dependencies and
+# compatibility. This assignment will stay FOSS (Free and Open Source
+# Software) and be provided in "AS IS" basis without any guarantees.
+# Any kinds of contribution, such as forks, PRs, Issues, are welcome
+# as long as it has the intention of constructive critisism or actual
+# contribution to source code. This piece of code, in no way, reflects
+# any of my plans for current and future projects and is meant solely
+# as a homework/assignment.
